9f9155a6357862ea57a5b5d807861b331e6289bc
tomoya06 <peng4766@gmail.com>
2023-04-16 18:51:27 +0800

    feat: cluster demo

---
+++ 002cluster/index.js
@@ -0,0 +1,28 @@
+import cluster from 'node:cluster';
+import os from 'node:os';
+
+if (cluster.isPrimary) {
+  console.log('i;m primary');
+
+  for (let i = 0; i < os.cpus().length; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('online', function (worker) {
+    console.log('Worker ' + worker.process.pid + ' is online');
+  });
+
+  cluster.on('exit', function (worker, code, signal) {
+    console.log(
+      'Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal,
+    );
+    console.log('Starting a new worker');
+    cluster.fork();
+  });
+} else {
+  console.log('i;m secondary');
+
+  setTimeout(() => {
+    process.exit(1);
+  }, 5000);
+}
